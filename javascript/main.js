08fd0863477bd2ec64059c4fd822ff3be0f5d704
Dan Day <danday@gmail.com>
2016-06-22 15:47:33 -0500

    experimenting

--- javascript/main.js
+++ javascript/main.js
@@ -25,14 +25,34 @@ acme.jsonLoadChain = function (selected) {
         console.log(selected);
         if (selected === json_data[x].name) {
           console.log("match_id", json_data[x].id)
+          var selectId = json_data[x].id;
         };
       }
+
+    console.log("xxx", x);
+    return ([selectId, x]);
     },
     // The second callback function will be invoked when you reject
     function(json_data) {
       console.log("API call not successful");
-    }
-  );
+
+//second promise THEN chain
+    }).then(function(idNum) {
+      console.log("ID NUMBER:", idNum[0], typeof(idNum[1]));
+    //   for (var x in json_data) {
+    //     console.log(json_data[x])
+    //     console.log(selected);
+    //     if (selected === json_data[x].name) {
+    //       console.log("match_id", json_data[x].id)
+    //       var selectId = json_data[x].id;
+    //     };
+    //   }
+    // return selectId;
+    },
+    // The second callback function will be invoked when you reject
+    function(json_data) {
+      console.log("API call not successful");
+  });
 
 };
 
